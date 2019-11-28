3eddb4987d732940237ebc5f824c5da07811d466
Claudio Busatto <c.busatto@visi-one.com>
2019-11-28 22:04:35 +0100

    Include comments

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const Sentry = require('@sentry/node');
 const { SENTRY_URL } = process.env;
 
-Sentry.init({
-  dsn: SENTRY_URL
-});
+// The only code required to allow Sentry to monitor thee application!
+Sentry.init({ dsn: SENTRY_URL });
 
-// This function is not defined, therefore Sentry is track a ReferenceError
+// This function is not defined, therefore Sentry is triggered
+// and an issue is created on the dashboard for this ReferenceError
 undefinedFunction();
